94e5cd8a27ed76fb45c4d72642918dc1ac5e93a9
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-09-17 00:51:14 -0500

    Cleanup header handling

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use crate::server::create_server;
 use crate::server::Request;
 
 fn main() {
-    create_server("127.0.0.1".to_string(), 8888, on_request);
+    create_server("127.0.0.1", 8888, on_request);
 }
 
 fn on_request(mut res:Request) {
     res.set_header("Connection", "close");
     res.set_header("Content-Type", "text/plain");
+    res.set_status(200, "OK");
     res.write_string("Hello");
     res.end();
     
--- src/server.rs
+++ src/server.rs
@@ -4,35 +4,67 @@ use std::net::TcpListener;
 use std::thread;
 use std::io::Read;
 
-fn write(mut stream:&TcpStream, data:&[u8], headers:&Vec<String>) {
-    let mut header = "HTTP/1.1 200 OK\r\n".to_string();
-    for value in headers.iter() {
-        if value.len() == 0 { continue; };
-        header += &(value.to_owned()+"\r\n");
+struct Header {
+    name: String,
+    value: String
+}
+impl Header {
+    pub fn new(header:&str, value:&str) -> Header {
+        Header {
+            name: header.to_string(),
+            value: value.to_string()
+        }
     }
-    header += "\r\n";
-    stream.write(header.as_bytes()).unwrap();
-    stream.write(data).unwrap();
 }
 
 pub struct Request {
     path: String,
     method: String,
     stream: TcpStream,
-    headers: Vec<String>
+    headers: Vec<Header>,
+    status_code: i32,
+    status_message: String
 }
 
 impl Request {
     pub fn new(stream:TcpStream, method:String, path:String) -> Request {
-        Request { method: method, path: path, stream: stream, headers: Vec::new()}
+        Request {
+            method: method,
+            path: path,
+            stream: stream,
+            headers: Vec::new(),
+            status_code: 200,
+            status_message: String::from("OK")
+        }
     }
-    pub fn write_string(&self, data:&str) {
-        write(&self.stream, data.to_string().as_bytes(), &self.headers);
+    fn write(&mut self, data:&[u8]) {
+        let mut header = ("HTTP/1.1 ".to_owned()+&self.status_code.to_string()+" "+self.status_message.as_str()+"\r\n").to_string();
+        for value in self.headers.iter() {
+            let key = value.name.to_owned()+": "+value.value.as_str();
+            header += &(key.to_owned()+"\r\n");
+        }
+        header += "\r\n";
+        self.stream.write(header.as_bytes()).unwrap();
+        self.stream.write(data).unwrap();
+    }
+    pub fn write_string(&mut self, data:&str) {
+        self.write(data.to_string().as_bytes());
     }
     pub fn set_header(&mut self, header:&str, value:&str) {
         // Are these header names and values valid?
-        // Are the headers being re-set?
-        self.headers.push(header.to_owned()+": "+value);
+        let new_header = Header::new(header, value);
+        for mut key in self.headers.iter_mut() {
+            if key.name == new_header.name {
+                *key = new_header;
+                return;
+            }
+        }
+        
+        self.headers.push(new_header);
+    }
+    pub fn set_status(&mut self, code:i32, msg:&str) {
+        self.status_code = code;
+        self.status_message = msg.to_string();
     }
     pub fn end(self) {
         drop(self.stream);
@@ -40,8 +72,8 @@ impl Request {
     
 }
 
-pub fn create_server(host:String, port:i32, on_request:fn(res:Request)) {
-    let listener = TcpListener::bind(host.clone()+":"+&port.to_string()).unwrap();
+pub fn create_server(host:&str, port:i32, on_request:fn(res:Request)) {
+    let listener = TcpListener::bind(host.to_string().to_owned()+":"+&port.to_string()).unwrap();
     println!("Server started on http://{}:{}/", host, port);
     for stream in listener.incoming() {
         thread::spawn(move || {
